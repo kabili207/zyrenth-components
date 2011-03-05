efdd279a643500bd91fb58e658f2a64ffd12bb70
Andrew Nagle <kabili@zyrenth.com>
2011-03-05 22:10:28 +0000

    More work on GTK AppointmentList

---
+++ Zyrenth GTK/Gtksharp/AppointmentListFrame.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Zyrenth
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AppointmentListFrame : Gtk.Bin
+	{
+		public AppointmentListFrame ()
+		{
+			this.Build ();
+		}
+	}
+}
+
