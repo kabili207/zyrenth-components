2b6d87ce9c13247437b5420c2031861ab1c38e2b
Andrew Nagle <kabili@zyrenth.com>
2011-03-05 22:37:47 +0000

    Added MySQL AES encryption and decryption methods to common library

--- Zyrenth Windows/Common.cs
+++ Zyrenth Windows/Common.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Zyrenth
 {
 	public static partial class Common
 	{
+		
+		private static Random random = new Random((int)DateTime.Now.Ticks);
+
+		/// <summary>
+		/// Gets a random number generator
+		/// </summary>
+		public static Random RandomNumberGen
+		{
+			get { return random; }
+		}
 		
 		/// <summary>
 		/// Restricts a value to a certain range
@@ -52,6 +66,15 @@ namespace Zyrenth
 			return false;
 		}
 		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="box"> A <see cref="Rectangle"/></param>
+		/// <param name="baseColor">A <see cref="Color"/></param>
+		/// <param name="mode">A <see cref="LinearGradientMode"/></param>
+		/// <returns>
+		/// A <see cref="LinearGradientBrush"/>
+		/// </returns>
 		public static LinearGradientBrush CreateGradient(Rectangle box, Color baseColor, LinearGradientMode mode) {
 			
 			// Create new colors that are slightly lighter and
@@ -66,6 +89,121 @@ namespace Zyrenth
 				Common.Clamp(baseColor.B - 35, 0, 255));
 			return new LinearGradientBrush(box, colorL, colorD, mode);	
 		}
+		
+		/// <summary>
+		/// Replaces any variation on line endings (CRLF, CR, LF) and 
+		/// replaces them with the operating system's line ending.
+		/// </summary>
+		/// <param name="text">The text to be sanitized</param>
+		/// <returns>A string with normalized line endings</returns>
+		public static string CleanLineEndings(string text)
+		{
+			return Regex.Replace(text, @"(?:\n|\r\n?)", Environment.NewLine, RegexOptions.Compiled);
+		}
+
+		/// <summary>
+		/// Truncates a string to the maximum length specified
+		/// </summary>
+		/// <param name="source">The input string</param>
+		/// <param name="length">The maximum length</param>
+		/// <returns>A string truncated to the length specified</returns>
+		public static string Truncate(string source, int length)
+		{
+			if (source.Length > length)
+			{
+				source = source.Substring(0, length);
+			}
+			return source;
+		}
+		
+		/// <summary>
+		/// Generates a random alpha-numeric string of the specifed length.
+		/// </summary>
+		/// <param name="size">The length of string to generate</param>
+		/// <returns>A randomly generated string</returns>
+		public static string RandomString(int length)
+		{
+			StringBuilder builder = new StringBuilder();
+			char[] pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+			for (int i = 0; i < length; i++)
+			{
+				builder.Append(pool[random.Next(pool.Length)]);
+			}
+
+			return builder.ToString();
+		}
+
+		internal static byte[] AESCreateKey(byte[] key, int keyLength)
+		{
+			// Create the Real Key with the given Key Length 
+			byte[] rkey = new byte[keyLength];
+
+			// XOR each byte of the Key given with the Real Key 
+			// until there's nothing left 
+			// This allows for keys longer than our Key Length 
+			// and pads short keys to the required length 
+			for (int i = 0; i < key.Length; i++)
+			{
+				rkey[i % keyLength] ^= key[i];
+			}
+
+			return rkey;
+		}
+
+		/// <summary>
+		/// Encrypts the string using MySQL's AES encryption technique and then
+		/// converts it to a Base64 encoded string.
+		/// </summary>
+		/// <param name="clearText">The string to be encrypted</param>
+		/// <param name="password">The password that should be used to encrypt the string</param>
+		/// <returns>An encrypted version of the specified string</returns>
+		public static string MysqlAesEncrypt(string clearText, string password)
+		{
+			byte[] clearData = Encoding.ASCII.GetBytes(clearText);
+			byte[] key = Encoding.ASCII.GetBytes(password);
+			MemoryStream ms = new MemoryStream();
+
+			Aes alg = Aes.Create();
+			alg.KeySize = 128;
+			alg.Mode = CipherMode.ECB;
+			alg.Padding = PaddingMode.PKCS7;
+			alg.Key = AESCreateKey(key, alg.KeySize / 8);
+			CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+
+			cs.Write(clearData, 0, clearData.Length);
+
+			cs.Close();
+			return Convert.ToBase64String(ms.ToArray());
+		}
+
+		/// <summary>
+		/// Takes a Base64 encoded string and decrypts it using MySQL's AES
+		/// decryption technique.
+		/// </summary>
+		/// <param name="cipherText">An encrypted base64 string</param>
+		/// <param name="password">The password</param>
+		/// <returns>The specified byte array decrypted</returns>
+		public static string MysqlAesDecrypt(string cipherText, string password)
+		{
+			byte[] cipherBytes = Convert.FromBase64String(cipherText);
+			byte[] key = Encoding.ASCII.GetBytes(password);
+			MemoryStream ms = new MemoryStream();
+
+			Aes alg = Aes.Create();
+			alg.KeySize = 128;
+			alg.Mode = CipherMode.ECB;
+			alg.Padding = PaddingMode.PKCS7;
+			alg.Key = AESCreateKey(key, alg.KeySize / 8);
+			CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+
+			cs.Write(cipherBytes, 0, cipherBytes.Length);
+
+			cs.Close();
+
+			byte[] newBytes = ms.ToArray();
+			return Encoding.ASCII.GetString(newBytes);
+		}
+
 	}
 }
 
